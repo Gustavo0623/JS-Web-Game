44137ce8eca0edc1822c81dbbd08c83bd1b9792c
Gustavo Martinez <Gus@Ronalds-MacBook-Pro.local>
2022-06-14 22:03:57 -0700

    moved items around and added more items. also attempted to make it more responsive to screen size may just continue off of earlier version if i need to later. :) note: display is optimal on desktop full screen

--- index.js
+++ index.js
@@ -18,10 +18,20 @@ function newImage(url, left, bottom){
     document.body.append(object)
     return object
 }
+
+function newImage2(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + '%'
+    object.style.bottom = bottom + '%'
+    document.body.append(object)
+    return object
+}
 // declaring function for items //
 
 function newItem(url, left, bottom){
-    let object = newImage(url, left, bottom)
+    let object = newImage2(url, left, bottom)
     
     object.addEventListener('dblclick', function(){
         object.remove()
@@ -39,13 +49,25 @@ let grass = horizon
 tile('assets/sky.png', 0, horizon, window.innerWidth/100, sky/100)
 tile('assets/grass.png', 0, 0, window.innerWidth/100, grass/100)
 
-newImage('assets/green-character.gif', 100, 100)
-newImage('assets/pine-tree.png', 450, 200)
-newImage('assets/tree.png', 200, 300)
-newImage('assets/pillar.png', 350, 100)
-newImage('assets/crate.png', 150, 200)
-newImage('assets/well.png', 500, 425)
+newImage2('assets/green-character.gif', 15, 5)
+newImage2('assets/pine-tree.png', 87, 29)
+newImage2('assets/pine-tree.png', 87, 0)
+newImage2('assets/pine-tree.png', 0, 29)
+newImage2('assets/pine-tree.png', 0, 0)
+newImage2('assets/tree.png', 12, 50)
+newImage2('assets/tree.png', 22, 50)
+newImage2('assets/tree.png', 32, 50)
+newImage2('assets/tree.png', 42, 50)
+newImage2('assets/tree.png', 52, 50)
+newImage2('assets/tree.png', 62, 50)
+newImage2('assets/tree.png', 72, 50)
+newImage2('assets/tree.png', 82, 50)
+newImage2('assets/pillar.png', 25, 13)
+newImage2('assets/pillar.png', 65, 13)
+newImage2('assets/crate.png', 25, 20)
+newImage2('assets/crate.png', 75, 20)
+newImage2('assets/well.png', 47, 20)
 
-newItem('assets/sword.png', 500, 405)
-newItem('assets/sheild.png', 165, 185)
-newItem('assets/staff.png', 600, 100)
+newItem('assets/sword.png', 38, 40)
+newItem('assets/sheild.png', 48, 40)
+newItem('assets/staff.png', 58, 40)
